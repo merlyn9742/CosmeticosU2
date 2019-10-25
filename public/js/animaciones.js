7fc348d70367767348927bb67f947fa07e4ec039
Moises Cervantes Salazar <moises220796@gmail.com>
2019-10-25 02:14:03 -0500

    Se corrigio el bug del navbar al deslizar

--- public/js/animaciones.js
+++ public/js/animaciones.js
@@ -1,3 +1,13 @@
+const etiquetaMenu = document.getElementsByClassName("contenedor-nav");
+var scroll;
+window.onscroll = function () {
+    if (window.scrollY > 0){
+        etiquetaMenu[0].classList.toggle('esconde-nav', window.scrollY > scroll);
+        scroll = window.scrollY;
+    }
+
+};
+
 var hamburguer = document.querySelector(".hamburger");
 hamburguer.addEventListener("click", function(){
     hamburguer.classList.toggle("is-active");
